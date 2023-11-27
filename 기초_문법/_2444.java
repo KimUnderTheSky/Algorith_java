658677c4935e126bd15b45fd55fe9a3412f2d43d
김천하 <cjsgksla@gimcheonhaui-MacBookAir.local>
2023-11-27 23:44:01 +0900

    별찍기

---
+++ 기초_문법/_2444.java
@@ -0,0 +1,42 @@
+package 기초_문법;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+//별찍기
+public class _2444 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> arr = new ArrayList<>(); // 별들
+
+
+        // 입력받기
+        String input = sc.nextLine();
+        int num = Integer.parseInt(input);
+
+        for (int i = 0; i < num; i++) {
+            StringBuilder line = new StringBuilder();
+            for (int j = 0; j < num - 1 - i; j++) {
+                line.append(" ");
+            }
+            for (int j = 0; j < i * 2 + 1; j++) {
+                line.append("*");
+            }
+            for (int j = 0; j < num - 1 - i; j++) {
+                line.append(" ");
+            }
+            arr.add(line.toString());
+        }
+
+        for(int i = num-2; i >= 0; i--){
+            arr.add(arr.get(i));
+        }
+
+        for (String s : arr) {
+            System.out.println(s);
+        }
+
+
+        sc.close();
+    }
+}
---
+++ 기초_문법/_27866.java
@@ -0,0 +1,21 @@
+package 기초_문법;
+//문자열, 문자
+import java.util.Scanner;
+
+public class _27866 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // 문자열 입력받기
+        String input = sc.nextLine();
+        // 순서 idx
+        String num = sc.next();
+        int idx = Integer.parseInt(num) - 1;
+
+        char result = input.charAt(idx);
+        System.out.println(result);
+
+
+        sc.close();
+    }
+}
