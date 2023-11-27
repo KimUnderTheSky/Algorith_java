c3b611677accbd6f587d57db088ab22d73bf0c89
김천하 <cjsgksla@gimcheonhaui-MacBookAir.local>
2023-11-27 15:58:38 +0900

    first commit

---
+++ Bronze_5/_10869.java
@@ -0,0 +1,21 @@
+package Bronze_5;
+
+import java.util.Scanner;
+
+public class _10869 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        // 입력받은 문자열을 공백기준으로 나눔
+        String[] values = input.split(" ");
+
+        System.out.println(Integer.parseInt(values[0]) + Integer.parseInt(values[1]));
+        System.out.println(Integer.parseInt(values[0]) - Integer.parseInt(values[1]));
+        System.out.println(Integer.parseInt(values[0]) * Integer.parseInt(values[1]));
+        System.out.println(Integer.parseInt(values[0]) / Integer.parseInt(values[1]));
+        System.out.println(Integer.parseInt(values[0]) % Integer.parseInt(values[1]));
+
+        sc.close();
+    }
+}
